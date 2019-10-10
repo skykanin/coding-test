528ea2be03b576dc922bfc3e605fd82c66f826b1
skykanin <3789764+skykanin@users.noreply.github.com>
2019-10-10 22:30:39 +0200

    Seperate styling out of render functions

--- src/coding_test/website/core.cljs
+++ src/coding_test/website/core.cljs
@@ -4,44 +4,57 @@
             [cljs.core.async :refer [<!]]
             [reagent.core :as r]))
 
-
 (def my-data (r/atom []))
 
-(def list-style
-  {:margin "auto"
-   :width "50%"
-   :border "3px solid black"
-   :padding "40px"})
+(def styling
+  {:list-style {:margin "auto"
+                :width "50%"
+                :border "3px solid black"
+                :padding "40px"}
+   :info-div {:color "white"
+              :text-align "left"
+              :border "2px solid black"
+              :position "relative"
+              :margin "10px"
+              :padding "15px"}
+   :info-title {:top "-10px"
+                :position "relative"
+                :border "2px solid red"
+                :width "35%"
+                :text-align "left"}
+   :info-list {:list-style-type "none"
+               :width "75%"
+               :position "relative"
+               :border "2px solid green"
+               :padding "0"}
+   :info-li {:display "inline-block"
+             :padding "15px"}})
 
 (go (let [response
-            (<! (http/get "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
-                          {:headers {"Client-Identifier" "IDENTIFIER"}}))
-            stations (:stations (:data (:body response)))]
-        (swap! my-data (constantly stations))))
+          (<! (http/get
+               "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
+               {:headers {"Client-Identifier" "IDENTIFIER"}}))
+          stations (:stations (:data (:body response)))]
+      (swap! my-data (constantly stations))))
+
+(defn list-item
+  "Renders a list element containing text information"
+  [[name val]]
+  [:li
+   {:style (:info-li styling)}
+   (str (apply str (rest (str name))) ": " val)])
 
 (defn bike-station
   "Renders a bike station element"
-  [{:keys [name capacity]}]
+  [data]
   [:div
-   {:style
-    {:color "white"
-     :text-align "left"
-     :border "2px solid black"
-     :position "relative"
-     :margin "10px"
-     :padding "15px"}}
-   [:h3 {:style
-         {:top "-20px"
-          :position "relative"
-          :border "2px solid red"
-          :width "35%"
-          :text-align "left"}}
-    name]
-   (str "Capacity: " capacity)])
-                     
+   {:style (:info-div styling)}
+   [:h3 {:style (:info-title styling)} (:name data)]
+   (into [:ul {:style (:info-list styling)}]
+         (map list-item (dissoc data :name)))])
 
 (defn render-ui []
-  (into [:div {:style list-style}]
+  (into [:div {:style (:list-style styling)}]
         (map bike-station @my-data)))
 
 (defn title-ui [title]
