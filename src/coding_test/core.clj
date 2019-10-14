71fe56869369e8c43e7f485f4153a830f6c6613c
skykanin <3789764+skykanin@users.noreply.github.com>
2019-10-14 02:43:49 +0200

    Delete empty files

--- src/coding_test/core.clj
+++
@@ -1,1 +0,0 @@
-(ns coding-test.core)
--- test/coding_test/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns coding-test.core-test
-  (:require [clojure.test :refer :all]
-            [coding-test.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
