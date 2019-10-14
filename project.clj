6531f9892ad8538bff1eb4a4606e0914b6d211bf
skykanin <3789764+skykanin@users.noreply.github.com>
2019-10-14 15:38:37 +0200

    Remove unfinished website

--- project.clj
+++ project.clj
@@ -11,17 +11,17 @@
                  [ring/ring-json "0.5.0"]
                  [ring/ring-mock "0.4.0"]]
   :main coding-test.calculator.core
-  :plugins [[lein-cljfmt "0.6.4"] [lein-figwheel "0.5.19"]]
-  :profiles {:dev {:dependencies [[cljs-http "0.1.46"]
-                                  [cider/piggieback "0.4.2"]
-                                  [com.bhauman/figwheel-main "0.2.3"]
-                                  [com.bhauman/rebel-readline-cljs "0.1.4"]
-                                  [org.clojure/clojurescript "1.10.339"]
-                                  [org.clojure/core.async "0.4.500"]
-                                  [reagent "0.9.0-rc1"]]
-                   :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
-                   :clean-targets ^{:protect false} ["target/public"]
-                   :resource-paths ["resources" "target"]}}
-  :test-paths ["test"]
-  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
-            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "website" "-r"]})
+  :plugins [[lein-cljfmt "0.6.4"]] ;[lein-figwheel "0.5.19"]
+  ;; :profiles {:dev {:dependencies [[cljs-http "0.1.46"]
+  ;;                                 [cider/piggieback "0.4.2"]
+  ;;                                 [com.bhauman/figwheel-main "0.2.3"]
+  ;;                                 [com.bhauman/rebel-readline-cljs "0.1.4"]
+  ;;                                 [org.clojure/clojurescript "1.10.339"]
+  ;;                                 [org.clojure/core.async "0.4.500"]
+  ;;                                 [reagent "0.9.0-rc1"]]
+  ;;                  :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
+  ;;                  :clean-targets ^{:protect false} ["target/public"]
+  ;;                  :resource-paths ["resources" "target"]}}
+  :test-paths ["test"])
+  ;; :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+  ;;           "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "website" "-r"]})
