1729cb49ef4c5e6231188a82f07daf805779b153
skykanin <3789764+skykanin@users.noreply.github.com>
2019-11-11 22:50:39 +0100

    Add more test cases for negative integers

--- test/coding_test/product_test.clj
+++ test/coding_test/product_test.clj
@@ -5,11 +5,16 @@
 
 ;; Validating the function input arguments for highest-prod
 (t/deftest valid-args
-  (t/is 120 (highest-prod '(1 2 3 4 5 6)))
-  (t/is 120 (highest-prod [1 2 3 4 5 6]))
-  (t/is 6 (highest-prod (list 1 3 -2)))
-  (t/is 75 (highest-prod [2 3 -5 -5]))
-  (t/is 120 (highest-prod [4 -3 -10 -1])))
+  (t/is (= 120 (highest-prod '(1 2 3 4 5 6))))
+  (t/is (= 120 (highest-prod [1 2 3 4 5 6])))
+  (t/is (= -6 (highest-prod (list 1 3 -2))))
+  (t/is (= 75 (highest-prod [2 3 -5 -5])))
+  (t/is (= 120 (highest-prod [4 -3 -10 -1])))
+  (t/is (= 1500 (highest-prod [4 -3 -10 5 -1 -30])))
+  (t/is (= 1200 (highest-prod [-9 8 -10 -15])))
+  (t/is (= 168 (highest-prod [-4 7 3 8])))
+  (t/is (= -90 (highest-prod [-10 -3 -5 -6 -20])))
+  (t/is (= 168 (highest-prod [1 -4 3 -6 7 0]))))
 
 (t/deftest invalid-args
   (rt/with-instrumentation `highest-prod
